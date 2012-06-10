33c6f4dbd95040d839773ffb368c13f0aa3fbe13
Christopher Harper <nomath4u@gmail.com>
2012-06-10 11:09:48 -0700

    Switched to case statements

--- shapes.rb
+++ shapes.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 
 
-################################################################################# ## Program Name: shapes.rb
+################################################################################# 
+# ## Program Name: shapes.rb
 # ## Author: Christopher Harper
 # ## Date: 6/14/2012
 # ## Description: Finds the area/volume of select shapes
@@ -11,21 +12,30 @@
 
 
 STDOUT.flush
-def selectShape
-	begin
-		puts "[1] Circle \n"
-		puts "[2] Rectangle \n"
-		puts "[3] Trapezoid \n"
-		puts "[4] Triangle \n"
-        	puts "Select shape:"
-		shape = gets.chomp.to_i
-		if shape == 1 or shape == 2 or shape == 3 or shape == 4
-			exitLoop = 1
-		else 
-			puts "Invalid selection"
+exit_shapeloop = FALSE
+exit_calcloop = FALSE
+until exit_shapeloop == TRUE do
+	puts "[1] Circle \n"
+	puts "[2] Rectangle \n"
+	puts "[3] Trapezoid \n"
+	puts "[4] Triangle \n"
+	puts "Select shape:"
+	shape = gets.chomp.to_i         #Without the to_i the following case always fails
+	exit_shapeloop = case shape
+		when 1..4 then TRUE
+		else           FALSE
 		end
-	end until exitLoop == 1
-	puts shape
-end
+	end
+until exit_calcloop == TRUE do
+	puts "Select a calculation"
+	puts "[1]Area \n"
+	puts "[2]Volume \n"
+	puts "[3] Both \n"
+	calc = gets.chomp.to_i
+	exit_calcloop = case calc
+		when 1..3 then TRUE
+		else           FALSE
+		end
+	end
 
-selectShape
+	
